7fc6e4eede9fffa988486c28618d020336ecd583
zunaidhosse <138894191+zunaidhosse@users.noreply.github.com>
2025-06-25 02:41:40 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,56 @@
+// sw.js (Service Worker)
+
+const CACHE_NAME = 'carwash-loyalty-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/firebase.js',
+  '/images/icon-192.png',
+  '/images/icon-512.png',
+  'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+];
+
+// Install event: open cache and add files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: serve cached content if available
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // Not in cache - fetch from network
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event: clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
